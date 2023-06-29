120bbd3703d422a9dabcbb3fd9e35bb148e4ce94
skejajulkabir <522ajaj@gmail.com>
2023-06-30 01:35:52 +0600

    eider shara din kaj korlam... product posting er functionality thik kora holo... no errors till now

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -6,7 +6,7 @@ import VideoPost from './VideoPost'
 const Feed = () => {
   return (
     <>
-    <div className="w-full bg-slate-500 px-4">
+    <div className="w-full bg-slate-500 px-4 ">
 
       <CreatePost />
 
